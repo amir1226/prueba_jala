545c23ecd13eb087f22b46c3f063e50dc0264064
amir1226@gmail.com <amir1226@gmail.com>
2020-12-21 09:36:34 -0500

    Correct counting char to counting words

--- Backend/models/lyric_models.py
+++ Backend/models/lyric_models.py
@@ -15,6 +15,6 @@ def count_words_and_lines(model: countIn):
     text_to_count = model.text
     lines = text_to_count.split("\n")
     lines_count = len(lines)
-    words_count_aux = list(map(lambda line: len(line.replace(" ", "")), lines))
+    words_count_aux = list(map(lambda line: len(line.split()), lines))
     words_count = sum(words_count_aux)
     return(lines_count, words_count)
